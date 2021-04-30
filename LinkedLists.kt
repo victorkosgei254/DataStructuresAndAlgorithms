0a47a841ec9dd8c6757577f4271c5c2df013378c
victor <victorkosgei254@gmail.com>
2021-04-30 06:24:01 +0300

    Linked List implementation, addNode, count

--- LinkedLists.kt
+++ LinkedLists.kt
@@ -1,13 +1,45 @@
-class Node<Int>() {
-    var data:Int? = null
+class Node<Int>(data: Int) {
+    var data:Int? = data
     var next:Node<Int>?=null
-    constructor(data:Int) : this() {
-        this.data = data
-    }
-
+    var previous:Node<Int>?=null
 }
 
 class LinkedLists<Int>{
     private var head:Node<Int>?= null
-    var isEmpty:Boolean = head == null 
+    var isEmpty:Boolean = head == null
+
+    fun first():Node<Int>? = head
+
+
+    fun last():Node<Int>?{
+        var node = head
+        if(node != null){
+            while (node?.next != null) node = node?.next
+            return node
+        }else   return null
+    }
+
+
+    fun add( data:Int){
+        var newNode  = Node(data)
+        var lastNode = last()
+        if (lastNode != null){
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        }else head=newNode
+
+
+    }
+
+    fun count(): kotlin.Int {
+        var node = head
+        if(node!=null){
+            var counter = 1
+            while(node?.next != null){
+                node=node?.next //increment to the next node
+                counter += 1
+            }
+            return counter
+        }else return 0
+    }
 }
--- Main.kt
+++ Main.kt
@@ -1,6 +1,7 @@
 package  main
 
 import FizzBuzz
+import LinkedLists
 import MapExample
 import SortItems
 
@@ -20,4 +21,9 @@ fun main(args0:Array<String>){
 
     val items = mutableListOf<Int>(1,2,3,4,5,6,7,8,9,0)
     FizzBuzz(items)
+
+    var linked = LinkedLists<Int>()
+    linked.add(4)
+    linked.add(9)
+    println(linked.count())
 }
